8cdf35cc7569cb8de8cb3cf2c6fbd759b78a8eaf
idanbe <idan_beck@walla.com>
2015-12-09 21:22:40 +0200

    go to settings activity by press button

--- Ex02/app/src/main/java/com/example/administrator/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/example/administrator/ex02/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.administrator.ex02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button strat,setting;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //init views
+        strat = (Button) findViewById(R.id.button_start);
+        setting = (Button) findViewById(R.id.button_sttings);
+
+
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to settings activity
+                Intent intent = new Intent(v.getContext(), settings.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
